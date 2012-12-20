55e02b1a6c3b26a217b7d62f0e0c35124809a2d7
ken <ken.frederick@gmx.de>
2012-12-21 00:39:53 +0100

    re-added src/frederickk.js

--- distribution/FrederickkPaper.js
+++ distribution/FrederickkPaper.js
@@ -1,4 +1,76 @@
 /**
+ *	
+ *	frederickkPaper.js
+ *	v0.2q
+ *	https://github.com/frederickk/frederickkPaper
+ *
+ *	25. November 2012
+ *
+ *	Ken Frederick
+ *	ken.frederick@gmx.de
+ *
+ *	http://cargocollective.com/kenfrederick/
+ *	http://kenfrederick.blogspot.com/
+ *	
+ *	
+ *	A collection of methods/functions that i find useful
+ *	specifically for application within PaperJs (http://paperjs.org/) 
+ *	most of which are based on my Frederickk library for Processing
+ *	(http://github.com/frederickk/frederickk)
+ *
+ *	Not all of the code in here was created by me
+ *	but credit and links are given where credit is due
+ *
+ *	Additional information and demos can be found here
+ *	http://kenfrederick.blogspot.de/2012/12/frederickkpaper.html
+ *
+ *
+ *	This library is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU Lesser General Public
+ *	License as published by the Free Software Foundation; either
+ *	version 2.1 of the License, or (at your option) any later version.
+ *	
+ *	http://creativecommons.org/licenses/LGPL/2.1/
+ *	
+ *	This library is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *	Lesser General Public License for more details.
+ *	
+ *	You should have received a copy of the GNU Lesser General Public
+ *	License along with this library; if not, write to the Free Software
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *	
+ */
+
+
+
+// ------------------------------------------------------------------------
+/**
+ *
+ *	REQUIRED LIBRARIES!
+ *
+ *	PaperJs @ http://paperjs.org/
+ *	JQuery @  http://jquery.com/download/
+ *
+ */
+
+
+
+// ------------------------------------------------------------------------
+var frederickkPaper = frederickkPaper || {};
+
+
+
+(function() {
+	console.log('\nfrederickkPaper.js');
+	console.log('v.0.2a');
+	console.log('https://github.com/frederickk/frederickkPaper');
+	console.log('ken.frederick@gmx.de');
+	console.log('------------------------------------\n');
+})();
+
+/**
  *  
  *	Core.js
  *	v0.2a
@@ -2119,18 +2191,6 @@ frederickkPaper.F3D.FPath3 = this.FPath3 = Path.extend({
 		return this;
 	}
 
-
-
-	// // ------------------------------------------------------------------------
-	// return {
-	// 	Line: function() {
-	// 		return new Path(
-	// 			Point.read(arguments),
-	// 			Point.read(arguments)
-	// 		);
-	// 	}
-	// }
-
 	
 
 });
@@ -2646,7 +2706,7 @@ frederickkPaper.F3D.FScene3D = this.FScene3D = function() {
 			50,
 			1,
 			this.bounds.depth,
-			this.bounds.depth*4
+			this.bounds.depth*4 //*2
 		);
 	};
 
---
+++ src/frederickk.js
@@ -0,0 +1,72 @@
+/**
+ *	
+ *	frederickkPaper.js
+ *	v0.2q
+ *	https://github.com/frederickk/frederickkPaper
+ *
+ *	25. November 2012
+ *
+ *	Ken Frederick
+ *	ken.frederick@gmx.de
+ *
+ *	http://cargocollective.com/kenfrederick/
+ *	http://kenfrederick.blogspot.com/
+ *	
+ *	
+ *	A collection of methods/functions that i find useful
+ *	specifically for application within PaperJs (http://paperjs.org/) 
+ *	most of which are based on my Frederickk library for Processing
+ *	(http://github.com/frederickk/frederickk)
+ *
+ *	Not all of the code in here was created by me
+ *	but credit and links are given where credit is due
+ *
+ *	Additional information and demos can be found here
+ *	http://kenfrederick.blogspot.de/2012/12/frederickkpaper.html
+ *
+ *
+ *	This library is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU Lesser General Public
+ *	License as published by the Free Software Foundation; either
+ *	version 2.1 of the License, or (at your option) any later version.
+ *	
+ *	http://creativecommons.org/licenses/LGPL/2.1/
+ *	
+ *	This library is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *	Lesser General Public License for more details.
+ *	
+ *	You should have received a copy of the GNU Lesser General Public
+ *	License along with this library; if not, write to the Free Software
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *	
+ */
+
+
+
+// ------------------------------------------------------------------------
+/**
+ *
+ *	REQUIRED LIBRARIES!
+ *
+ *	PaperJs @ http://paperjs.org/
+ *	JQuery @  http://jquery.com/download/
+ *
+ */
+
+
+
+// ------------------------------------------------------------------------
+var frederickkPaper = frederickkPaper || {};
+
+
+
+(function() {
+	console.log('\nfrederickkPaper.js');
+	console.log('v.0.2a');
+	console.log('https://github.com/frederickk/frederickkPaper');
+	console.log('ken.frederick@gmx.de');
+	console.log('------------------------------------\n');
+})();
+
