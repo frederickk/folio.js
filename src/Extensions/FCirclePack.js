267b25ae4c7c993e5f6fb02c614b96c5bf9afb5d
Ken Frederick <frederickk@google.com>
2016-04-06 14:40:04 -0700

    refactored FCirclePack and added ability to drag item

--- src/Extensions/FCirclePack.js
+++ src/Extensions/FCirclePack.js
@@ -42,7 +42,7 @@
  * @return {Object}
  *
  */
-folio.FCirclePack = function(circleItems, iterations) {
+folio.FCirclePack = function(circleItems, properties) {
     // ------------------------------------------------------------------------
     //
     // Properties
@@ -53,13 +53,20 @@ folio.FCirclePack = function(circleItems, iterations) {
         : (circleItems == null)
             ? []
             : circleItems;
-    iterations = (iterations != undefined)
-        ? iterations
-        : 11;
 
-    var dampingAmt = 0.1; // the lower the slower
-    var padding = 0;
-    var target = view.center;
+    properties = properties || {};
+    var iterations = properties.iterations || 11;
+    var dampingAmt = properties.damping || 0.1; // the lower the slower
+    var padding = properties.padding || 0;
+    var target = properties.target || view.center;
+
+    var hitOptions = {
+        stroke    : true,
+        fill      : true,
+        tolerance : 5
+    };
+    var isMousedown = false;
+    var dragCircle;
 
 
 
@@ -70,21 +77,23 @@ folio.FCirclePack = function(circleItems, iterations) {
     // ------------------------------------------------------------------------
     function update() {
         circleItems = circleItems.sort(FSort.distanceToCenter);
-        var pp = new Point();
 
-        // Push items away from each other
+        var pp = new Point();
+        var ci, cj;
+        var dx, dy;
+        var radius, dist;
         for (var i = circleItems.length - 1; i >= 0; --i) {
-            var ci = circleItems[i];
+            ci = circleItems[i];
 
             for (var j = i + 1; j < circleItems.length; j++) {
-                var cj = circleItems[j];
+                cj = circleItems[j];
 
                 if (i == j) {
                     continue;
                 }
 
-                var dx = cj.position.x - ci.position.x;
-                var dy = cj.position.y - ci.position.y;
+                dx = cj.position.x - ci.position.x;
+                dy = cj.position.y - ci.position.y;
 
                 // this alogroithm is designed for circles, so we assume
                 // every object is either a circle or a square.
@@ -93,23 +102,23 @@ folio.FCirclePack = function(circleItems, iterations) {
                 // http://en.wikipedia.org/wiki/Packing_problem
                 // hence why we just halve the "width" in
                 // order to get the object's radius
-                var r = (ci.bounds.size.width / 2) + (cj.bounds.size.width / 2) + padding;
-                var d = (dx * dx) + (dy * dy);
+                radius = (ci.bounds.size.width / 2) + (cj.bounds.size.width / 2) + padding;
+                dist = (dx * dx) + (dy * dy);
 
-                if (d < (r * r) - 0.01) {
+                if (dist < (radius * radius) - 0.01) {
                     pp.x = dx;
                     pp.y = dy;
                     pp = pp.normalize(1.0);
-                    pp = pp.multiply((r - Math.sqrt(d)) * 0.5);
+                    pp = pp.multiply((radius - Math.sqrt(dist)) * 0.5);
 
                     try {
-                        // if (cj != this.dragCircle) {
-                        cj.position.x += pp.x;
-                        cj.position.y += pp.y;
+                        // if (cj != dragCircle) {
+                            cj.position.x += pp.x;
+                            cj.position.y += pp.y;
                         // }
-                        // if (ci != this.dragCircle) {
-                        ci.position.x -= pp.x;
-                        ci.position.y -= pp.y;
+                        // if (ci != dragCircle) {
+                            ci.position.x -= pp.x;
+                            ci.position.y -= pp.y;
                         // }
                     }
                     catch(err) {
@@ -122,24 +131,21 @@ folio.FCirclePack = function(circleItems, iterations) {
             }
         }
 
-        // push items toward center
         var damping = dampingAmt / parseInt(iterations);
-
+        var c;
         for (var i = 0; i < circleItems.length; i++) {
-            var c = circleItems[i];
-            // if (c == this.dragCircle) continue;
-            pp.x = c.position.x - target.x;
-            pp.y = c.position.y - target.y;
-            pp = pp.multiply(damping);
-            // }
+            c = circleItems[i];
+            if (c == dragCircle) {
+                continue;
+            }
+            else {
+                pp.x = c.position.x - target.x;
+                pp.y = c.position.y - target.y;
+                pp = pp.multiply(damping);
+            }
             c.position.x -= pp.x;
             c.position.y -= pp.y;
         }
-
-        // if (this.dragCircle && this._mouseEvent) {
-        //  this.dragCircle.x = this._mouseEvent.offsetX;//stage.mouseX;
-        //  this.dragCircle.y = this._mouseEvent.offsetY;//stage.mouseY;
-        // }
     }
 
 
@@ -211,6 +217,34 @@ folio.FCirclePack = function(circleItems, iterations) {
     var getItem = function(index) {
         return circleItems[index];
     };
+
+
+    // ------------------------------------------------------------------------
+    //
+    // Events
+    //
+    // ------------------------------------------------------------------------
+    function resetEvents() {
+        isMousedown = false;
+        dragCircle = null;
+    }
+    window.addEventListener('onblur', resetEvents);
+    window.addEventListener('mouseup', resetEvents);
+    window.addEventListener('mouseout', resetEvents);
+
+    window.addEventListener('mousedown', function(event) {
+        isMousedown = true;
+        var hitResult = project.hitTest([event.clientX, event.clientY], hitOptions);
+        if (hitResult && hitResult.item) {
+            dragCircle = hitResult.item;
+        }
+    });
+
+    window.addEventListener('mousemove', function(event) {
+        if (isMousedown && dragCircle) {
+            dragCircle.position = [event.clientX, event.clientY];
+        }
+    });
 
 
 
